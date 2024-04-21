7fd10ea53a717ed221c26009b60bafdeb771cf7f
camelia <126248163+cameliacharef@users.noreply.github.com>
2024-04-21 23:19:12 +0200

    Update CoordonneesIncorrectesException.java

--- src/CoordonneesIncorrectesException.java
+++ src/CoordonneesIncorrectesException.java
@@ -1,5 +1,4 @@
-//CHAREF CAMELIA 21202057
-//BEN MIMOUN SARAH 28713241
+
 public class CoordonneesIncorrectesException extends Exception{
     public CoordonneesIncorrectesException(String s){
         super(s);
