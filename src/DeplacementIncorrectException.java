41453a8617c932cdcb734cbcd361bc33b81de71c
camelia <126248163+cameliacharef@users.noreply.github.com>
2024-04-21 23:19:23 +0200

    Update DeplacementIncorrectException.java

--- src/DeplacementIncorrectException.java
+++ src/DeplacementIncorrectException.java
@@ -1,5 +1,3 @@
-//CHAREF CAMELIA 21202057
-//BEN MIMOUN SARAH 28713241
 public class DeplacementIncorrectException extends Exception{
     public DeplacementIncorrectException(String s){
         super(s);
